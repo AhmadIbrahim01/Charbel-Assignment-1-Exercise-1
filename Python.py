669a8a2e006519e3faade723ec8f5c09df829699
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-10-28 00:27:12 +0200

    Update Python.py

--- Python.py
+++ Python.py
@@ -5,7 +5,7 @@ for i in range(rows, 0, -1):
     print(" "*i + "*" * stars + " "*i)
     stars += 2
     
-stars -= 4
+stars -= 2
 for i in range(2, rows + 3):
-    print(" "*i + "*" * stars + " "*i)
     stars -= 2
+    print(" "*i + "*" * stars + " "*i)
